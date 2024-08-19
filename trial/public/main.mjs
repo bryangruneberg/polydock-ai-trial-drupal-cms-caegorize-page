f82bdcf7d5350742796f94fbad3f32a3e6e2af67
Matt Glaman <27012-mglaman@users.noreply.drupalcode.org>
2024-08-19 13:16:42 +0000

    Add trial experience

---
+++ trial/public/main.mjs
@@ -0,0 +1,8 @@
+import { registerWorker } from './drupal-cgi-worker.mjs'
+import { defineTrialManagerElement } from "./trial-manager.mjs";
+
+defineTrialManagerElement()
+registerWorker(
+  `${window.location.origin}/service-worker.mjs`,
+  `${window.location.origin}/service-worker.js`
+)
