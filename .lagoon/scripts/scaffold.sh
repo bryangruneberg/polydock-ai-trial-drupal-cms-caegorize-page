16d25a04f639691e3d79871c90990c4240d59957
Blaize Kaye <blaize.kaye@gmail.com>
2025-02-20 11:11:47 +1300

    Adds further scaffolding work (#10)
    * Adds further scaffolding
    
    * Build in dockerfile
    
    * updates volume mount
    
    * conditionally scaffold
    
    * Changes structure of scaffold
    
    * more scaffolding
    
    * updates settings.php
    
    * Testing DB install
    
    * Adds drush si to post-rollout
    
    ---------
    
    Co-authored-by: Blaize Kaye <blaize.kaye@amazee.io>

--- .lagoon/scripts/scaffold.sh
+++ .lagoon/scripts/scaffold.sh
@@ -6,15 +6,24 @@ cd /app
 
 # Check if the flag file exists
 if [ ! -f "$FLAG_FILE" ]; then
+    echo "*** NO SCAFFOLD FILE FOUND ***"
     git config --global --add safe.directory /app
-    composer install --no-dev
-    cp -r /app/vendor/drupal/cms/web/profiles/drupal_cms_installer /app/web/profiles/
-    cp /app/drush/Commands/contrib/drupal_integrations/assets/* /app/web/sites/default
-    cp /app/.lagoon/assets/* /app/web/sites/default
-    mv /app/web/sites/default/initial.settings.php /app/web/sites/default/settings.php
-    
+    if [ -z "$LAGOON_KUBERNETES" ]; then
+        echo "Not running in Lagoon - we will try install"
+        composer install --no-dev
+        cp -r /app/vendor/drupal/cms/web/profiles/drupal_cms_installer /app/web/profiles/
+        cp /app/drush/Commands/contrib/drupal_integrations/assets/* /app/web/sites/default
+    fi    
     # Create the flag file to indicate the script has run
+    echo "About to create $FLAG_FILE"
     touch "$FLAG_FILE"
 else
+    echo "*** SCAFFOLD FILE FOUND ***"
     echo "The initialization script has already run."
+fi
+
+# here we attempt to copy the details
+if [ ! -f "/app/web/sites/default/settings.php" ]; then
+    cp /app/.lagoon/assets/* /app/web/sites/default
+    mv /app/web/sites/default/initial.settings.php /app/web/sites/default/settings.php
 fi
